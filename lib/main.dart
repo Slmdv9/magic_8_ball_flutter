399cff507d610bfda7f28f81355e695b2ea9e1ca
Samuel <samuel9lmdv@gmail.com>
2024-02-28 16:41:17 +0100

    Step 3 - Create a Statefull Widget
    Show a image in a Center Widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,24 @@ class BallPage extends StatelessWidget {
           backgroundColor: Colors.blue.shade900,
         ),
         backgroundColor: Colors.blue,
-        body: Container(),
+        body: const Ball(),
       ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
